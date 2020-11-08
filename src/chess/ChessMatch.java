f42d4ae813783c2b749a1e0c1082e71cb52df226
Saulo de Oliveira Junior <mroluas@gmail.com>
2020-11-08 20:43:23 -0300

    Fix - Better treatment to promotion
    Fix - Better treatment to promotion

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -83,7 +82,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Nao existe peca para ser promovida!");
 		}
 		if(!type.equals("B") && !type.equals("C") && !type.equals("T") && !type.equals("Q")){
-			throw new InvalidParameterException("Tipo de peça invalida para promocao!");
+			return promoted;
 		}
 
 		Position pos = promoted.getChessPosition().toPosition();
