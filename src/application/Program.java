e4da216da1b101ed5fbd1055f780eac367844617
Saulo de Oliveira Junior <mroluas@gmail.com>
2020-11-08 21:41:25 -0300

    Removing unused include
    Removing unused include

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,4 @@
 package application;
-
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
-
-import chess.ChessException;
-import chess.ChessMatch;
-import chess.ChessPiece;
-import chess.ChessPosition;
-
 public class Program {
 
 	public static void main(String[] args) {
